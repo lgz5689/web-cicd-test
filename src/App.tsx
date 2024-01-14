2dc75e7214da38d1b262c4cb320e962c09f03ceb
kevin <1026838160@qq.com>
2024-01-14 17:24:31 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const App = () => {
+  // eslint has error
+  // const [first, setfirst] = useState("");
+
+  // eslint not error
+  const [first] = useState("");
+
+  // auto format
+  // if (first) {
+  //   console.log(first);
+  // }
+
+  if (first) {
+    console.log(first);
+  }
+
+  return <div>Hello World</div>;
+};
+
+export default App;
