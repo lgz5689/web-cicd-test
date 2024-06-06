2a0d881454794e3875aba593c48b9309c6bf2982
kevin <1026838160@qq.com>
2024-06-07 00:44:59 +0800

    feat: 1

---
+++ scripts/size-report.ts
@@ -0,0 +1,32 @@
+import fs from "node:fs/promises";
+import process from "node:process";
+import path from "node:path";
+import prettyBytes from "pretty-bytes";
+
+const distPath = path.resolve(__dirname, "..", "dist");
+
+async function calculateSize(directory: string) {
+  const files = await fs.readdir(directory);
+
+  let totalSize = 0;
+
+  for (const file of files) {
+    const filePath = path.join(directory, file);
+    const stats = await fs.stat(filePath);
+
+    if (stats.isFile()) {
+      totalSize += stats.size;
+    } else if (stats.isDirectory()) {
+      totalSize += await calculateSize(filePath);
+    }
+  }
+
+  return totalSize;
+}
+
+async function main() {
+  const distSize = await calculateSize(distPath);
+  process.stdout.write(prettyBytes(distSize));
+}
+
+main();
